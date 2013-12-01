fd1ee3069f34da758ad5e9b2e6bcfe3c1398d8c8
Peter de croos <peter.decroos@smartshoot.com>
2013-11-30 22:59:46 -0800

    upload all the things

--- mocks/engineio-server.js
+++ mocks/engineio-server.js
@@ -4,26 +4,25 @@ var http = require('http');
 var Reiki = require('../lib/index.js');
 
 var server = function() {
-  return http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('hello world');
-    res.end('');
-  });
+  return http.Server();
 };
 
-var reiki = module.exports = new Reiki({server: server()});
+
+var reiki = new Reiki({server: server()});
 
 //you should update this to include the stream api, for now I'll use
 //the traditional calback style for the mock
-reiki.server.on('connection', function(socket) {
+/*
+reiki.io.on('connection', function(socket) {
+  console.log('socket.send: ' , socket.send);
   socket.on('echo', function(msg) {
     console.log('from the server ' , arguments);
-    socket.send(msg);
+    //socket.send(msg);
   });
   socket.on('message', function() {
     console.log('triggered message, see ', arguments);
   });
 });
+*/
 
-
-
+module.exports = reiki;
--- test/engineio-server.js
+++ test/engineio-server.js
@@ -7,39 +7,39 @@ describe('engineioServer', function() {
   describe('websocket server', function() {
     var reiki;
     var connectionUrl;
-    before(function() {
+    beforeEach(function() {
       connectionUrl = configs.host + ':' + configs.port;
       reiki = require('../mocks/engineio-server.js');
-      reiki.listen(8080);
+      
     });
 
-    after(function() {
-      reiki.server.close();
+    afterEach(function() {
+      //reiki.server.close();
+
     });
 
-
+/*
     it('should return hello world for a standard http request', function(done) {
+      reiki.listen(configs.port);
       request('http://' + connectionUrl, function(err, response, body) {
         should.exist(body);
         body.should.equal('hello world');
         done();
       });
     });
-
-    it('should return "hello world" in response to me sending it', function(done) {
-      console.log('listening');
+*/
+    it('should return "hello world" in response to me sending it "hello world" ', function(done) {
       reiki.listen(configs.port);
-      console.log('sweet');
-      var client = require('engine.io-client')('ws://' + connectionUrl);
-      console.log('client emit:' + client.emit);
-      client.onopen = function() {
-        client.onmessage = function(data) {
-          console.log('go data: ', data);
-        };
-        client.onclose = function() {};
-        //client.emit('message', 'hello world');
-        client.send('shittzzz');
-      };
+      var client = require('socket.io-client')('http://' + connectionUrl);
+      /*
+      client.on('connect', function() {
+        client.on('message', function(data) {
+          console.log('from client ' , data);
+
+        });
+        //client.emit('message', 'foo');
+      });
+      */
     });
   });
 });
